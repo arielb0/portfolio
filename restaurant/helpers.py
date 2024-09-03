0f8bd4cfa6e65e27f943ad1166495ebbfa41fa4f
arielb0 <arielscull90@gmail.com>
2024-09-03 13:10:36 -0400

    Included print sentence to see default locale

--- restaurant/helpers.py
+++ restaurant/helpers.py
@@ -1,5 +1,6 @@
 from scull_suite.settings import BASE_DIR
 import onnxruntime as onnx
+import locale
 
 '''
     TODO: Create a class with this function and instantiate on models.
@@ -9,7 +10,7 @@ def make_inference(text: str):
         Given a a input text predicts if review is positive or negative,
         using a Open Neural Network Exchange (ONNX) format
     '''
-    
+    print(f'This is the default locale on Render: {locale.getdefaultlocale()}')
     session = onnx.InferenceSession(f'{BASE_DIR}/restaurant/en_review_classificator.onnx', providers=['CPUExecutionProvider'])
     input_name = session.get_inputs()[0].name
     label_name = session.get_outputs()[0].name
