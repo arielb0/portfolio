b547d346346bbc20f1b62bb7cfd3599253717353
arielb0 <arielscull90@gmail.com>
2025-01-28 08:39:49 -0500

    Added translation to Accounts and Bazaar applications

--- scull_suite/settings.py
+++ scull_suite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework_simplejwt',
     'rest_framework',
     'accounts',
     'note',
